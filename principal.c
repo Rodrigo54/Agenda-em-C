4323824d9d8534ac1eefad037a15853aa3bfb4a6
Rodrigo Alves Mesquita <rodrigo54mix@gmail.com>
2015-01-30 15:30:30 -0200

    Update principal.c

--- principal.c
+++ principal.c
@@ -1,9 +1,9 @@
-/* AGENDA EM LIMGUAGUEM C
+/** AGENDA EM LIMGUAGUEM C
  *
- * Agenda feita para obtenção de nota parcial na mateira de 'Algoritmos e Estrutura de Dados'.
+ * Agenda feita para obtenÃ§Ã£o de nota parcial na mateira de 'Algoritmos e Estrutura de Dados'.
  *
- * Autor: Rodrigo Alves Mesquita
- * Aluno do 3° periodo do curso de Bacharel em Ciências da Computação na Faculdade Pitágoras
+ * @autor Rodrigo Alves Mesquita
+ * Aluno do 3Â° periodo do curso de Bacharel em CiÃªncias da ComputaÃ§Ã£o na Faculdade PitÃ¡goras
  *
  */
 
@@ -11,9 +11,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <conio.h>
-#include <ctype.h>//contém declarações para manipulação de caracteres.
-#include <locale.h> //para o programa aceitar acentuação
-#define TAM 100 //TAM recebe o valor de 100 inteiros
+#include <ctype.h>      //contÃ©m declaraÃ§Ãµes para manipulaÃ§Ã£o de caracteres.
+#include <locale.h>     //para o programa aceitar acentuaÃ§Ã£o
+#define TAM 100         //TAM recebe o valor de 100 inteiros
 
 typedef struct Agenda{
         char nome[50];
@@ -22,28 +22,28 @@ typedef struct Agenda{
 }contatos;
 
 /*============  Prototipos  ===============*/
-void Incluir(void);    // função para Incluir dados
-void Listar(void);     // função para Mostrar os contatos ja incluidos
-void Organizar(void);  // função para Colocar em ordem os contatos da agenda
-void Pesquisar(void);  // função para Pesquisar contatos
-char AddMais();        // função para Adicionar mais contatos na agenda
-void Formata(void);    // função para Apaga todos os contatos na agenda
+void Incluir(void);    // funÃ§Ã£o para Incluir dados
+void Listar(void);     // funÃ§Ã£o para Mostrar os contatos ja incluidos
+void Organizar(void);  // funÃ§Ã£o para Colocar em ordem os contatos da agenda
+void Pesquisar(void);  // funÃ§Ã£o para Pesquisar contatos
+char AddMais();        // funÃ§Ã£o para Adicionar mais contatos na agenda
+void Formata(void);    // funÃ§Ã£o para Apaga todos os contatos na agenda
 
 /*============  Variaveis Globais  =============*/
-static int qtd = 0;     // qtd é uma variavel do tipo estatica que conta a quantidade de contatos incritos
-contatos max[TAM];      // agora max[100] é um vetor do tipo contatos definido com o typedef
-FILE *arq;              // declarar a variável(*arq) como ponteiro do arquivo(FILE)
+static int qtd = 0;     // qtd Ã© uma variavel do tipo estatica que conta a quantidade de contatos incritos
+contatos max[TAM];      // agora max[100] Ã© um vetor do tipo contatos definido com o typedef
+FILE *arq;              // declarar a variÃ¡vel(*arq) como ponteiro do arquivo(FILE)
 
 /*========  Incluir Contatos na Agenda  ===========*/
 void Incluir(void){
      int cont = 0;   //cont sera a variavel contadora
      int retorno;    //retorno seve para definir se fwrite funcionou
-     char op = 's';  //seve para definir a opção na função AddMais()
+     char op = 's';  //seve para definir a opÃ§Ã£o na funÃ§Ã£o AddMais()
 
      arq = fopen("agenda.txt", "a");// fopen cria arquivo de entrada
-     if (arq == NULL){//caso o sistema não consiga criar o arquivo
-        printf ("Erro!\nO arquivo da lista não pode ser aberto!\n");//sera mostrada esta mensagen
-        getch();// espera que o usuário pressione uma tecla
+     if (arq == NULL){//caso o sistema nÃ£o consiga criar o arquivo
+        printf ("Erro!\nO arquivo da lista nÃ£o pode ser aberto!\n");//sera mostrada esta mensagen
+        getch();// espera que o usuÃ¡rio pressione uma tecla
         exit(1);//caso esse erro ocorra este comando encerra o programa
      }
      while ((cont < TAM) && (op == 's')){
@@ -63,7 +63,7 @@ void Incluir(void){
                printf("\n Gravacao ok! ");
            }
            cont++;//enquanto cont for menor 100 adiciona mais um contato
-           op = AddMais();//chama a função que pergunta se deseja inserir novos contatos
+           op = AddMais();//chama a funÃ§Ã£o que pergunta se deseja inserir novos contatos
            qtd++;//acrecenta 1 contato a mais
      }
      fclose (arq);//fecha o arquivo agenda.txt
@@ -83,26 +83,26 @@ char AddMais() {
 /*=====================  Apagar tudo  ====================*/
 void Formata() {
     /* w+ abre um arquivo para leitura e escrita.
-    Se o arquivo não existir, o sistema operacional tentará criá-lo.
-    Se o arquivo existir, todo o seu conteúdo será substituído pelo novo conteúdo.*/
+    Se o arquivo nÃ£o existir, o sistema operacional tentarÃ¡ criÃ¡-lo.
+    Se o arquivo existir, todo o seu conteÃºdo serÃ¡ substituÃ­do pelo novo conteÃºdo.*/
     arq = fopen("agenda.txt","w+"); //recria um arquivo limpo
     printf("\n\tLista limpa!\n ");
     fclose(arq);//fecha o arquivo agenda.txt
-    getch();// espera que o usuário pressione uma tecla
+    getch();// espera que o usuÃ¡rio pressione uma tecla
 }
 
 /*=============  Organizar em ordem alfabetica  ==========*/
-void Organizar(void){//algoritmo de ordenação do tipo selection sort (ordenação por seleção)
-     contatos vet; //vet é um variavel do tipo struct Agenda
+void Organizar(void){//algoritmo de ordenaÃ§Ã£o do tipo selection sort (ordenaÃ§Ã£o por seleÃ§Ã£o)
+     contatos vet; //vet Ã© um variavel do tipo struct Agenda
      int aux,i,j,k,retorno;
-     char *str, *str2, *straux;//o asterisco [*] indica que a variavel é uma string
+     char *str, *str2, *straux;//o asterisco [*] indica que a variavel Ã© uma string
      aux = qtd; // aux recebe a quantidade de contatos inscritos
-     // Rotina de ordenação
+     // Rotina de ordenaÃ§Ã£o
      for (i = 0 ; i < aux ; i++){
          str = max[i].nome;//a variavel str recebe o primeio contato
          for (j = i+1 ; j < aux ; j++){
              str2 = max[j].nome;//a variavel str2 recebe o segundo contato
-             //strcmp faz a comparação entre as strings onde 0 são strings iguais
+             //strcmp faz a comparaÃ§Ã£o entre as strings onde 0 sÃ£o strings iguais
              if ( strcmp ( str, str2 ) > 0 ){
                  vet = max[i];
                  max[i] = max[j];
@@ -112,9 +112,9 @@ void Organizar(void){//algoritmo de ordenação do tipo selection sort (ordenação 
      }
      remove("agenda.txt");//Apaga o Arquivo
      arq = fopen("agenda.txt", "w+");//Recria um arquivo limpo
-     if (arq == NULL){//caso o sistema não consiga criar o arquivo
+     if (arq == NULL){//caso o sistema nÃ£o consiga criar o arquivo
         printf (" ERRO!\n ");// sera mostrada esta mensagens
-        getch();// espera que o usuário pressione uma tecla
+        getch();// espera que o usuÃ¡rio pressione uma tecla
         exit(1);//caso esse erro ocorra este comando encerra o programa
      }
      for (k = 0 ; k < aux ; k++){
@@ -125,7 +125,7 @@ void Organizar(void){//algoritmo de ordenação do tipo selection sort (ordenação 
      }
      fclose(arq);//fecha o arquivo agenda.txt
      printf("\n Lista Ordenada!\n ");
-     getch();//espera que o usuário pressione uma tecla
+     getch();//espera que o usuÃ¡rio pressione uma tecla
 }
 
 /*============== Pesquisar contato pelo nome =====================*/
@@ -134,9 +134,9 @@ void Pesquisar(void){
      char nome[50],op;//A variavel nome se refere ao nome a ser pesquisado
 
      arq = fopen("agenda.txt", "r");//fopen abre o arquivo no modo leitura "r"
-     if (arq == NULL){//caso o SO não consiga abrir o arquivo
-         printf (" Erro!\nO arquivo da lista não pode ser aberto! \n");//sera mostrada esta mensagens
-         getch();//espera que o usuário pressione uma tecla
+     if (arq == NULL){//caso o SO nÃ£o consiga abrir o arquivo
+         printf (" Erro!\nO arquivo da lista nÃ£o pode ser aberto! \n");//sera mostrada esta mensagens
+         getch();//espera que o usuÃ¡rio pressione uma tecla
          exit(1);//caso esse erro ocorra este comando encerra o programa
      }
      printf (" Digite o nome: ");// para procurar contato pelo nome
@@ -152,10 +152,10 @@ void Pesquisar(void){
      i++;
      retorno = fread(&max[i], sizeof(contatos), 1, arq);//fread vai ler o proximo contato
      }
-     if(cont == 0){//se strcmp não encontrar strings iguais
+     if(cont == 0){//se strcmp nÃ£o encontrar strings iguais
         printf("Nao ha contatos com este nome!\n ");//sera mostrada essa mensagen
      }
-     getch();//espera que o usuário pressione uma tecla
+     getch();//espera que o usuÃ¡rio pressione uma tecla
      fclose(arq);//fecha o arquivo agenda.txt
 }
 
@@ -164,9 +164,9 @@ void Listar(void){
     int i = 0, retorno;
 
     arq = fopen("agenda.txt", "r");//fopen abre o arquivo no modo leitura "r"
-    if (arq == NULL){//caso o SO não consiga abrir o arquivo
-       printf ("Erro!\nO arquivo da lista não pode ser aberto!\n");//sera mostrada esta mensagens
-       getch();//espera que o usuário pressione uma tecla
+    if (arq == NULL){//caso o SO nÃ£o consiga abrir o arquivo
+       printf ("Erro!\nO arquivo da lista nÃ£o pode ser aberto!\n");//sera mostrada esta mensagens
+       getch();//espera que o usuÃ¡rio pressione uma tecla
        exit(1);//caso esse erro ocorra este comando encerra o programa
     }
     retorno = fread(&max[i], sizeof(contatos), 1, arq);//fread le apenas 1 contato do arquivo
@@ -178,21 +178,21 @@ void Listar(void){
       retorno = fread(&max[i], sizeof(contatos), 1, arq);//fread vai ler o proximo contato
     }
     printf(" \n\n %d Contatos salvos!\n ", i);
-    getch();//espera que o usuário pressione uma tecla
+    getch();//espera que o usuÃ¡rio pressione uma tecla
     fclose(arq);//fecha o arquivo agenda.txt
 }
 
 /*========== Sobre os Autores ================*/
 void Sobre(void){
     printf("\n\n\t\tAGENDA EM LINGUAGUEM C\n\n");
-    printf("\tTrabalho para obtenção de nota Parcial\n\tNa Disciplina de Algoritmos e Estrutura de Dados\n");
+    printf("\tTrabalho para obtenÃ§Ã£o de nota Parcial\n\tNa Disciplina de Algoritmos e Estrutura de Dados\n");
     printf("\tIntegrantes da Equipe:\n\t\tRodrigo Alves\n\t\tAdailton\n\t\tEduardo Ramos\n\t\tRoseane Brito");
-    getch();//espera que o usuário pressione uma tecla
+    getch();//espera que o usuÃ¡rio pressione uma tecla
 }
 
 /*=====================   Menu   =======================*/
 void menu(void){
-    char op;//variavel de opção
+    char op;//variavel de opÃ§Ã£o
     do{
         system("cls");// limpar tela
         printf("\n\n\t\tAGENDA EM LINGUAGUEM C\n");
@@ -224,25 +224,25 @@ void menu(void){
                 main();
                 break;
             default:
-                printf("\a Digite uma opção valida\n");
-                getch();//espera que o usuário pressione uma tecla
+                printf("\a Digite uma opÃ§Ã£o valida\n");
+                getch();//espera que o usuÃ¡rio pressione uma tecla
         }
     }
     while (op);
 }
 
-/*=========== Função Principal ================*/
+/*=========== FunÃ§Ã£o Principal ================*/
 int main (){
     int var;
     system("title AGENDA");
     system("color 1f");// Define a o plano de Fundo Azul Marinho e o texto em Branco
     setlocale(LC_ALL, "Portuguese");//Define no console o idioma Portugues
     printf("\n\n\t\tAGENDA EM LINGUAGUEM C\n\n");
-    printf("\tVeja todas as funções disponiveis no menu.\n");
-    printf("\tUse os numeros para selecionar a opção desejada.\n");
-    printf("\tPressione qualquer tecla para continuar ou\n\tEspaço para sair do programa agora.\n\t");
+    printf("\tVeja todas as funÃ§Ãµes disponiveis no menu.\n");
+    printf("\tUse os numeros para selecionar a opÃ§Ã£o desejada.\n");
+    printf("\tPressione qualquer tecla para continuar ou\n\tEspaÃ§o para sair do programa agora.\n\t");
     var=getch();
-    if(var == 32){ exit(0);}// 32 corresponde a espaço no teclado conforme Tabela ASCII
-menu();// chamando a funçao menu para a main
+    if(var == 32){ exit(0);}// 32 corresponde a espaÃ§o no teclado conforme Tabela ASCII
+menu();// chamando a funÃ§ao menu para a main
 system("pause");
 }
